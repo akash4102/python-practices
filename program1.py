961cf0f6c9073614faa5e944725e4064fac1c28c
Akash Verma <aakashverma4102@gmail.com>
2022-04-06 13:47:06 +0530

    add-comments

--- program1.py
+++ program1.py
@@ -1,4 +1,11 @@
+#  this is a program to capitalize all the first letter in the string 
+# first we take input from the user
 s=(str)(input())
+
+# then we run a loop and split whole string by using .split() function
 for x in s.split():
+    #  then we simply use x.capitalize() function to capital letter in the string
     s=s.replace(x,x.capitalize())
+
+# and finally we print the stirng and we got the result
 print(s)
